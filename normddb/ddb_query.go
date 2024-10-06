a5c53178a76327b627d32867f5a2cf1bebb1e864
Axel Engström <axel.engstrom@northvolt.com>
2024-10-06 11:08:11 +0200

    rename cursor -> keycondition

--- normddb/ddb_query.go
+++ normddb/ddb_query.go
@@ -5,19 +5,18 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
-	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
 	expression2 "github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
 	dynamodbv2 "github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
-type QueryPattern string
+// type QueryPattern string
 
-const (
-	PartitionKeyPagination = "pkPagination"
-	SortedPagination       = "skPagination"
-	DirectLookup           = "directLookup"
-)
+// const (
+// 	PartitionKeyPagination = "pkPagination"
+// 	SortedPagination       = "skPagination"
+// 	DirectLookup           = "directLookup"
+// )
 
 type Getter struct {
 	ddb *dynamodbv2.Client
@@ -42,8 +41,8 @@ func (g *Getter) LookupMany(ctx context.Context) (DynamoEntity, error) {
 type Querier struct {
 	ddb *dynamodbv2.Client //? separate the client from this type and instead use different custom ddb client?
 
-	table       TableDefinition
-	startCursor Cursor
+	table   TableDefinition
+	keyCond KeyCondition
 
 	//internal, not exposed to user
 	lastCursor map[string]types.AttributeValue
@@ -56,28 +55,34 @@ type queryOptions struct {
 	// because if you don't know what you're doing you may introduce race conditions.
 	eventuallyConsistent bool
 	pageSize             int32
-	scanForward          bool
+	descending           bool
+	indexName            *string
 	filter               expression2.ConditionBuilder
 }
 
 const defaultPageSize = 10
 
-type Cursor struct {
+type KeyCondition struct {
 	partition any
-	strategy  SortKeyStrategy // is it really a cursor, since it doesn't change when scrolling?
+	strategy  SortKeyStrategy
 }
 
-func NewCursor(partition any, strategy SortKeyStrategy) Cursor {
-	return Cursor{
+func NewKeyCondition(partition any, strategy SortKeyStrategy) KeyCondition {
+	return KeyCondition{
 		partition: partition,
 		strategy:  strategy,
 	}
 }
 
 // todo make first two arguments here part of the dynamodb client interface. ddb Clients should be able to return a Querier and a Getter.
-func NewQuerier(ddb *dynamodbv2.Client, table TableDefinition, cursor Cursor) *Querier {
+//
+// todo: replace TableDefinition argument with "Index" interface that has "Table()" method, and "IndexName()" method.
+// The base table can export methods like "Primary()", "GSI_1()" that return the index.
+// Then you can call them via "artifacts.Table.GSI_1()"
+func NewQuerier(ddb *dynamodbv2.Client, table TableDefinition, kc KeyCondition) *Querier {
 	return &Querier{
-		ddb: ddb,
+		ddb:     ddb,
+		keyCond: kc,
 		opts: &queryOptions{
 			pageSize: defaultPageSize,
 		},
@@ -90,10 +95,10 @@ type QueryResult struct {
 }
 
 func (q *Querier) Next(ctx context.Context) (*QueryResult, error) {
-	b := expression.NewBuilder()
-	key := expression2.KeyEqual(expression2.Key(q.table.KeyDefinitions.PartitionKey.Name), expression2.Value(q.startCursor.partition))
-	if q.startCursor.strategy != nil {
-		key.And(q.startCursor.strategy(q.table.KeyDefinitions.SortKey.Name))
+	b := expression2.NewBuilder()
+	key := expression2.KeyEqual(expression2.Key(q.table.KeyDefinitions.PartitionKey.Name), expression2.Value(q.keyCond.partition))
+	if q.keyCond.strategy != nil {
+		key.And(q.keyCond.strategy(q.table.KeyDefinitions.SortKey.Name))
 	}
 	b.WithKeyCondition(key)
 
@@ -108,7 +113,7 @@ func (q *Querier) Next(ctx context.Context) (*QueryResult, error) {
 
 	res, err := q.ddb.Query(ctx, &dynamodbv2.QueryInput{
 		TableName:                 &q.table.Name,
-		IndexName:                 nil, // todo, if passing an index (not a table) that is a GSI, this is populated
+		IndexName:                 q.opts.indexName,
 		KeyConditionExpression:    expr.KeyCondition(),
 		FilterExpression:          expr.Filter(),
 		ProjectionExpression:      expr.Projection(), // todo implement
@@ -116,7 +121,7 @@ func (q *Querier) Next(ctx context.Context) (*QueryResult, error) {
 		ExpressionAttributeNames:  expr.Names(),
 		ConsistentRead:            ptr(!q.opts.eventuallyConsistent),
 		Limit:                     ptr(q.opts.pageSize),
-		ScanIndexForward:          ptr(q.opts.scanForward),
+		ScanIndexForward:          ptr(q.opts.descending),
 		ExclusiveStartKey:         q.lastCursor,
 	})
 	if err != nil {
@@ -149,8 +154,8 @@ func (q *Querier) WithEventuallyConsistentReads() *Querier {
 	return q
 }
 
-func (q *Querier) WithScanForward() *Querier {
-	q.opts.scanForward = true
+func (q *Querier) WithDescending() *Querier {
+	q.opts.descending = true
 	return q
 }
 
@@ -159,8 +164,14 @@ func (q *Querier) WithPageSize(limit int) *Querier {
 	return q
 }
 
+func (q *Querier) WithGSI(indexName string) *Querier {
+	q.opts.indexName = &indexName
+	return q
+}
+
 // Filter based on entity type.
-// queries on indices can pass entity filters by default to its query constructor
+//
+// todo queries on indices should pass entity filters by default to its query constructor
 // todo add more filters
 func (q *Querier) WithEntityFilter(typ string) *Querier {
 	q.opts.filter = q.opts.filter.And(expression2.Equal(expression2.Name("meta.type"), expression2.Value(typ)))
@@ -182,13 +193,13 @@ var Table = TableDefinition{
 	},
 }
 
-func main() {
+func _() {
 	var ddb *dynamodbv2.Client
 
-	q := NewQuerier(ddb, Table, NewCursor("state#123", BeginsWith("lol"))).
-		WithScanForward().
+	q := NewQuerier(ddb, Table, NewKeyCondition("state#123", BeginsWith("lol"))).
+		WithDescending().
 		WithPageSize(10).
-		WithEntityFilter("exampleentity")
+		WithEntityFilter("ExampleEntity")
 
 	ctx := context.Background()
 	res, err := q.Next(ctx)
